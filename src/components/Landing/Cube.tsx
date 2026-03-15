6b4a8974339791792900ea99e3974934da7f60c8
saur0w <sthapliyal085@gmail.com>
2026-03-15 13:24:46 +0530

    "Image in the Scene"

--- src/components/Landing/Cube.tsx
+++ src/components/Landing/Cube.tsx
@@ -8,38 +8,14 @@ import * as THREE from "three";
 
 export default function Cube() {
     const groupRef = useRef<THREE.Group>(null);
-    const earthRef = useRef<THREE.Mesh>(null);
-    const sunRef = useRef<THREE.Mesh>(null);
-
-    const earthTexture = useTexture("/textures/earth.jpg");
-    const sunTexture = useTexture("/textures/sun.jpg");
-
-    useFrame(() => {
-        if (groupRef.current) {
-            groupRef.current.rotation.y += 0.019;
-        }
-
-        if (earthRef.current) {
-            earthRef.current.rotation.y += 0.001;
-        }
-
-        if (sunRef.current) {
-            sunRef.current.rotation.y += 0.0001;
-        }
-
-    });
+    const img1 = useTexture("/images/look.jpg");
+    
     return (
         <>
             <group>
-                <mesh position={[0, 0, 0]} ref={sunRef}>
-                    <sphereGeometry args={[2, 64, 64]} />
-                    <meshBasicMaterial map={sunTexture} />
-                </mesh>
-            </group>
-            <group ref={groupRef}>
-                <mesh position={[-5, 0, 0]} ref={earthRef}>
-                    <sphereGeometry args={[0.5, 64, 64]} />
-                    <meshBasicMaterial map={earthTexture} />
+                <mesh>
+                    <planeGeometry args={[4, 3]} />
+                    <meshBasicMaterial map={img1} />
                 </mesh>
             </group>
         </>
--- src/components/Landing/Scene.tsx
+++ src/components/Landing/Scene.tsx
@@ -2,19 +2,16 @@
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from 'react';
-import Cube from "./Cube";
-import { Stars } from "@react-three/drei";
+import Obj from "./Cube";
 
 export default function Scene() {
     return (
         <Canvas
-            camera={{ position: [0, -10, -10] }}
+            camera={{ position: [0, 0, 10] }}
         >
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[10, 10, 5]} intensity={1} />
-            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
+          <ambientLight intensity={1} />
           <Suspense fallback={null}>
-              <Cube />
+              <Obj />
           </Suspense>
       </Canvas>  
     );
