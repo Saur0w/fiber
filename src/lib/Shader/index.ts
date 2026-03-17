a4fdfa01fe7ac37e8b058a40432070c54e9fbdbf
saur0w <sthapliyal085@gmail.com>
2026-03-17 19:16:35 +0530

    "Fragment Shader"

--- src/lib/Shader/index.ts
+++ src/lib/Shader/index.ts
@@ -1,11 +1,14 @@
 export const vertexShader = `
   uniform float uVelocity;
   varying vec2 vUv;
+  varying float vWorldX;
 
   void main() {
     vUv = uv;
 
     vec4 worldPos = modelMatrix * vec4(position, 1.0);
+    
+    vWorldX = worldPos.x;
     float wave = sin(worldPos.x * 0.9) * uVelocity * 0.006;
 
     worldPos.y += wave * 0.5;          
@@ -19,7 +22,17 @@ export const fragmentShader = `
   uniform sampler2D uTexture;
   varying vec2 vUv;
 
+  varying float vWorldX;
+
   void main() {
-    gl_FragColor = texture2D(uTexture, vUv);
+    vec4 texColor = texture2D(uTexture, vUv);
+    
+    float grayValue = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
+    vec3 grayscale = vec3(grayValue);
+    
+    float distanceFromCenter = abs(vWorldX);
+    float colorFactor = smoothstep(2.5, 4.5, distanceFromCenter);
+    vec3 finalColor = mix(texColor.rgb, grayscale, colorFactor);
+    gl_FragColor = vec4(finalColor, texColor.a);
   }
 `;
